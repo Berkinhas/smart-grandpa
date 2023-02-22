fa356a04510ff9cb80159a7a1a28442dea7cf556
Berkinhas <mateus35ba@gmail.com>
2023-02-22 19:39:24 -0300

    Estilização do tab bottom

--- front/App.js
+++ front/App.js
@@ -2,6 +2,7 @@ import 'react-native-gesture-handler';
 import { createStackNavigator } from "@react-navigation/stack";
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { StyleSheet, Text, View, Image, TouchableOpacity  } from 'react-native';
 
 //TELAS DE NAVEGAÇÃO
 import { LoginScreen } from './src/screens/SignIn/login';
@@ -26,17 +27,177 @@ import React from 'react';
 const Stack = createStackNavigator();
 const Tab = createBottomTabNavigator();
 
+const CustomTabBarButton = ({children, onPress}) => (
+  <TouchableOpacity 
+    style={{
+      top: -30,
+      justifyContent: 'center',
+      alignItems: 'center',
+      ...styles.shadow
+    }}
+    onPress={onPress}
+  >
+    <View style={{
+      width: 70,
+      height: 70,
+      borderRadius: 35,
+      backgroundColor: '#9C80BE'
+    }}>
+      {children}
+    </View>
+  </TouchableOpacity>
+
+)
+
 function Tabs(){
   return(
-    <Tab.Navigator>
-      <Tab.Screen name='Home' component={HomeScreen}/>
-      <Tab.Screen name='Search' component={SearchScreen}/>
-      <Tab.Screen name='Publication' component={PubScreen}/>
-      <Tab.Screen name='Chat' component={ChatScreen}/>
-      <Tab.Screen name='Profile' component={ProfileScreen}/>
+    <Tab.Navigator
+      screenOptions={{
+        tabBarShowLabel: false,
+        headerShown: false,
+        tabBarStyle: {
+          position: 'absolute',
+          bottom: 25,
+          left: 20,
+          right: 20,
+          elevation: 0,
+          backgroundColor: '#ffffffff',
+          borderRadius: 15,
+          height: 90,
+          ...styles.shadow
+        }
+      }}
+    
+    >
+      <Tab.Screen name='Home' component={HomeScreen} options={{
+        tabBarIcon: ({focused}) => (
+          <View style={{alignItems: 'center', justifyContent: 'center', top: 10}}>
+            <Image
+              source={require('./assets/botao-de-inicio.png')}
+              resizeMode='contain'
+              style={{
+                width: 25,
+                height: 25,
+                tintColor: focused ? '#9C80BE' : '#748c94'
+              }}
+            />
+            <Text
+            style={{color: focused ? '#9C80BE' : '#748c94', fontSize:12 }}
+            
+            >
+            Home  
+            </Text>
+          </View>
+        ),
+      }}/>
+      <Tab.Screen name='Search' component={SearchScreen}
+        options={{
+          tabBarIcon: ({focused}) => (
+            <View style={{alignItems: 'center', justifyContent: 'center', top: 10}}>
+              <Image
+                source={require('./assets/search-interface-symbol.png')}
+                resizeMode='contain'
+                style={{
+                  width: 25,
+                  height: 25,
+                  tintColor: focused ? '#9C80BE' : '#748c94'
+                }}
+              />
+              <Text
+              style={{color: focused ? '#9C80BE' : '#748c94', fontSize:12 }}
+              
+              >
+                Procurar
+              </Text>
+            </View>
+          ),
+        }}
+      />
+      <Tab.Screen name='Publication' component={PubScreen}
+        options={{
+          tabBarIcon: ({focused}) => (
+        
+            <Image
+              source={require('./assets/plus.png')}
+              resizeMode="contain"
+              style={{
+                width: 30,
+                height: 30,
+                tintColor: "#fff"
+              }}
+            />
+          ),
+          
+          tabBarButton: (props) => (
+            <CustomTabBarButton {...props}/>
+            
+          )
+          
+        }}
+        
+      />
+      <Tab.Screen name='Notification' component={ChatScreen}
+        options={{
+          tabBarIcon: ({focused}) => (
+            <View style={{alignItems: 'center', justifyContent: 'center', top: 10}}>
+              <Image
+                source={require('./assets/notification.png')}
+                resizeMode='contain'
+                style={{
+                  width: 25,
+                  height: 25,
+                  tintColor: focused ? '#9C80BE' : '#748c94'
+                }}
+              />
+              <Text
+              style={{color: focused ? '#9C80BE' : '#748c94', fontSize:12 }}
+              
+              >
+              Notificações  
+              </Text>
+            </View>
+          ),
+        }}
+      />
+      <Tab.Screen name='Profile' component={ProfileScreen}
+        options={{
+          tabBarIcon: ({focused}) => (
+            <View style={{alignItems: 'center', justifyContent: 'center', top: 10}}>
+              <Image
+                source={require('./assets/user.png')}
+                resizeMode='contain'
+                style={{
+                  width: 25,
+                  height: 25,
+                  tintColor: focused ? '#9C80BE' : '#748c94'
+                }}
+              />
+              <Text
+              style={{color: focused ? '#9C80BE' : '#748c94', fontSize:12 }}
+              
+              >
+              Perfil  
+              </Text>
+            </View>
+          ),
+        }}
+      />
     </Tab.Navigator>
   )
 }
+
+const styles = StyleSheet.create({
+  shadow: {
+    shadowColor: '#7F5DF0',
+    shadowOffset: {
+      width: 0,
+      height: 10,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.5,
+    elevation: 5
+  }
+})
 
 export default function App() {
   return (

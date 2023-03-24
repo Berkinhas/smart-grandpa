04250cf075fe58fa2ab7b1d10e9c83ba34341b16
Berkinhas <mateus35ba@gmail.com>
2023-03-24 00:57:52 -0300

    alterações home, settings, search, post

--- front/src/screens/Home/home.js
+++ front/src/screens/Home/home.js
@@ -1,15 +1,18 @@
-import * as React from 'react';
+import React, { useState } from 'react';
 import { Text, View, TextInput, TouchableOpacity, ScrollView, ImageBackground } from 'react-native';
 import * as Animatable from 'react-native-animatable';
 import { FiraSans_500Medium, useFonts } from '@expo-google-fonts/fira-sans';
 import Icon from 'react-native-vector-icons/Ionicons';
 import { Card, Title, Paragraph, Button } from 'react-native-paper';
 import { MaterialIcons } from '@expo/vector-icons';
+import { Modal } from 'react-native';
 
 import styleHome from './styleHome.js' 
 
 
 export function HomeScreen({ navigation }) {
+
+  const [modalVisible, setModalVisible] = React.useState(false);
 
        const [email, onChangeEmail] = React.useState('');
        const [senha, setPassword] = React.useState('');
@@ -32,7 +35,46 @@ export function HomeScreen({ navigation }) {
       style={styleHome.background}
       resizeMode="cover"
     >
+    
       <View style={styleHome.container}>
+      	 <TouchableOpacity style={styleHome.settingsButton} onPress={() => setModalVisible(true)}>
+  		<MaterialIcons name="settings" size={30} color="white" />
+	 </TouchableOpacity>
+	 <TouchableOpacity style={styleHome.buttonNotification} onPress={() => {          
+            navigation.navigate('NotificationScreen')    }}>
+  		<MaterialIcons name="notifications" size={30} color="white" />
+	 </TouchableOpacity>
+<Modal animationType="fade" visible={modalVisible}>
+  <View style={styleHome.modalContainer}>
+    <TouchableOpacity onPress={() => setModalVisible(false)} style={styleHome.closeButton}>
+      <MaterialIcons name="close" size={24} color="white" />
+    </TouchableOpacity>
+    <View style={styleHome.modalContent}>
+      <Text style={styleHome.modalTitle}>Configurações</Text>
+      <Text style={styleHome.modalDescription}>O que deseja fazer (nome)?</Text>
+      <View style={styleHome.optionsContainer}>
+        <TouchableOpacity style={styleHome.option}>
+          <MaterialIcons name="logout" size={24} color="black" />
+          <Text style={styleHome.optionText}>Log out</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styleHome.option}>
+          <MaterialIcons name="help" size={24} color="black" />
+          <Text style={styleHome.optionText}>Assistência</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styleHome.option}>
+          <MaterialIcons name="description" size={24} color="black" />
+          <Text style={styleHome.optionText}>Termos e condições</Text>
+        </TouchableOpacity>
+      </View>
+      <TouchableOpacity onPress={() => setModalVisible(false)} style={styleHome.backButton}>
+        <MaterialIcons name="keyboard-arrow-left" size={24} color="black" />
+        <Text style={styleHome.backButtonText}>Voltar</Text>
+      </TouchableOpacity>
+    </View>
+  </View>
+</Modal>
+
+
         <View style={styleHome.textContainer}>
           <Text style={styleHome.welcomeText}>Bem-vindo (nome)!</Text>
           <View style={styleHome.iconContainer}>
---
+++ front/src/screens/Search/styleSearch.js
@@ -0,0 +1,107 @@
+import { StyleSheet } from 'react-native';
+
+const styleSearch = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#F5F5F5',
+  },
+  header: {
+    height: 64,
+    backgroundColor: '#0077B5',
+    marginTop: 40,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: '#FFFFFF',
+  },
+  noJobsContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  noJobsText: {
+    fontSize: 20,
+    color: '#333333',
+  },
+  jobsContainer: {
+    paddingBottom: 20,
+  },
+  jobCard: {
+    backgroundColor: '#FFFFFF',
+    flexDirection: 'row',
+    padding: 16,
+    marginHorizontal: 16,
+    marginVertical: 8,
+    borderRadius: 4,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+  },
+  logoContainer: {
+    width: 64,
+    height: 64,
+    marginRight: 16,
+  },
+  logo: {
+    flex: 1,
+    width: null,
+    height: null,
+resizeMode: 'contain',
+
+},
+jobDetailsContainer: {
+flex: 1,
+justifyContent: 'center',
+},
+jobTitle: {
+fontSize: 16,
+fontWeight: 'bold',
+marginBottom: 4,
+color: '#333',
+},
+company: {
+fontSize: 14,
+color: '#666',
+marginBottom: 4,
+},
+location: {
+fontSize: 14,
+color: '#666',
+marginBottom: 4,
+},
+applyButtonContainer: {
+width: 80,
+height: 32,
+borderRadius: 16,
+backgroundColor: '#0077b5',
+alignItems: 'center',
+justifyContent: 'center',
+},
+applyButtonText: {
+color: '#fff',
+fontSize: 14,
+fontWeight: 'bold',
+},
+noJobContainer: {
+flex: 1,
+justifyContent: 'center',
+alignItems: 'center',
+},
+noJobText: {
+fontSize: 20,
+fontWeight: 'bold',
+color: '#333',
+},
+
+
+})
+
+  export default styleSearch;
